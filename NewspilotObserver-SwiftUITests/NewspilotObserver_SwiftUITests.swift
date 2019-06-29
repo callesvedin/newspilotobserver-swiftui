d062dd9ca99da02c220f73a76f4c5b923ce4a5b5
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2019-06-29 21:04:42 +0200

    Initial Commit

---
+++ NewspilotObserver-SwiftUITests/NewspilotObserver_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  NewspilotObserver_SwiftUITests.swift
+//  NewspilotObserver-SwiftUITests
+//
+//  Created by carl-johan.svedin on 2019-06-29.
+//  Copyright © 2019 Infomaker Scandinavia AB. All rights reserved.
+//
+
+import XCTest
+@testable import NewspilotObserver_SwiftUI
+
+class NewspilotObserver_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
