4e5f9f47c64eee75e360d17f2f9fb7aa8e6500c4
Carl-Johan Svedin <carl-johan.svedin@infomaker.se>
2021-02-10 23:16:37 +0100

    Fixed commit mishap

--- iOS/Views/PageDetail/PageDetailView.swift
+++ iOS/Views/PageDetail/PageDetailView.swift
@@ -46,84 +46,10 @@ struct PageDetailView: NameableView {
                 BottomSheetView(isOpen: self.$infoViewShown,
                                 maxHeight: geometry.size.height * 0.7)
                 {
-                    InfoView(page:self.page)
+                    PageInfoView(page:self.page)
                 }
             }
             .connectionBanner()
-        }
-    }
-}
-
-
-struct InfoView: View {
-    let page:PageViewModel
-    
-    var body: some View {
-        #if !os(macOS)
-        UITableView.appearance().backgroundColor = .clear // tableview background
-        UITableViewCell.appearance().backgroundColor = .clear // cell background
-        #endif
-        return List {
-            KeyValueView(key:"Name", value:page.name)
-            KeyValueView(key:"Part", value:page.part)
-            KeyValueView(key:"Edition", value:page.edition)
-            KeyValueView(key:"Version", value:page.version)
-            KeyValueView(key:"Section", value:page.section)
-            KeyValueView(key:"Template", value:page.template)
-            KeyValueView(key:"Edition Type", value: page.editionType.stringValue)
-            KeyColorValueView(key:"Status", name:page.statusName, color: page.statusColor)
-            FlagsView(key:"Flags", flags:page.flags)
-                
-            
-        } //.background(Color. edgesIgnoringSafeArea(.all))
-    }    
-}
-
-//struct PageDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//
-//        let view = PageDetailView(page:PageViewModel(id: 1, pageNumber: 4, name: "Great page", section: "Section A",
-//                                                     part: "Part A", edition: "Edition 1", version: "Version 3", template: "A-Section",
-//                                                     editionType: .identical, statusName: "Ready",
-//                                                     statusColor: UIColor.green, flags:[UIImage(systemName: "star"), UIImage(systemName: "star.fill")], thumbUrl: nil, previewUrl: nil)
-//        )
-//        return Group {
-//            view.previewDevice(PreviewDevice(rawValue: "iPhone SE")).previewDisplayName("iPhone SE")
-//            view.previewDevice(PreviewDevice(rawValue: "iPhone XS Max")).previewDisplayName("iPhone XS Max")
-//        }
-//    }
-//}
-
-struct KeyValueView: View {
-    let key:String
-    let value:String?
-    
-    var body: some View {
-        HStack {
-            Text(self.key).bold()
-            Spacer()
-            if value != nil{
-                Text(self.value!)
-            }
-        }
-    }
-}
-
-struct KeyColorValueView: View {
-    let key:String
-    let name:String
-    let color:UIColor
-    
-    var body: some View {
-        HStack {
-            Text(self.key).bold()
-            Spacer()
-            Text(self.name).foregroundColor(.gray)
-            if color == .white {
-                Image.init(systemName: "circle").foregroundColor(Color.black)
-            }else {
-                Image.init(systemName: "circle.fill").foregroundColor(Color(color))
-            }
         }
     }
 }
